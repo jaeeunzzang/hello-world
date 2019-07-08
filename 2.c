c4e970ed622052c1004d8d847b379f13e1ef2973
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-08 11:09:40 +0900

    Update 2.c

--- 2.c
+++ 2.c
@@ -5,4 +5,16 @@
 
 void main()
 {
+  double rad; //반지름
+  double area;
+  
+  printf("반지름을 입력하세요(cm) : ");
+  scanf("%lf",&rad);
+  
+  area=3.14*pow(rad,2);
+  
+  printf("원의 면적은 %.2lf cm2입니다: ",area);
+}
+
+ 
   
