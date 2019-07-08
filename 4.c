150174b0a12d3b0d58b2fcd32ce16907b6895eb9
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-08 11:41:41 +0900

    Update 4.c

--- 4.c
+++ 4.c
@@ -8,9 +8,10 @@ void main()
   printf("한화 금액을 입력하세요 : ");
   scanf("%lf",&krw);
   
-  usd=krw*0.85;
-  jpy=krw*0.97;
+  usd=krw*0.00085;
+  jpy=krw*0.0919;
   
-  printf("%.2lf"원은 %.2lf 달러 %.2lf 엔입니다." ,krw,usd,jpy);
-         }
+  printf("%.2lf원은 %.2lf 달러 %.2lf 엔입니다." ,krw,usd,jpy);
+}
+         
          
