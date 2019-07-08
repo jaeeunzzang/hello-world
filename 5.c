2508130a24e9ef82666a83db5c8fbf1c4dbfa303
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-08 13:17:49 +0900

    Update 5.c

--- 5.c
+++ 5.c
@@ -1,1 +1,15 @@
+//연봉계산 프로그램
+
+#include <stdio.h>
+#define MONTH 12
+
+void main()
+{
+  int m_sal,y_sal;
+  
+  printf("월급을 입력하세요. : ");
+  scanf("%d",&m_sal);
+  y_sal= MONTH * m_sal;
+  printf("연봉은 %d 입니다.",y_sal);
+}
 
