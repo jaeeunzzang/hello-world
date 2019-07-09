bbaf383c7e9e13e7e35a28db7e2286244fd51cd6
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-09 10:15:38 +0900

    Update 8.c

--- 8.c
+++ 8.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+void main()
+{
+  double x;
+  double total;
+  
+  printf("x의 값을 입력하세요 : ");
+  scanf("%lf",&x);
+  
+  total=(x*x*x*3)-(x*x*7)+9;
+  
+  printf("다항식의 값은 %.4lf 입니다. ",total);
+}
+
+  
