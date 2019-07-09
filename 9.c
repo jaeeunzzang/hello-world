265b46f45e1fb383a8699fd759137da333161d02
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-09 11:15:02 +0900

    Update 9.c

--- 9.c
+++ 9.c
@@ -2,4 +2,13 @@
 
 void main()
 {
+  int time,min,sec;
   
+  printf("시간(초)를 입력해 주세여: ");
+  scanf("%d",&time);
+  min=time/60;
+  sec=time%60;
+  
+  printf("%d초는 %d분 %d 초 입니다.",time,min,sec);
+}
+
