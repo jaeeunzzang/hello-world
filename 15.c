221f3900f90a328e1f8d43708fbebac0a8dc2a38
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-16 14:53:14 +0900

    Update 15.c

--- 15.c
+++ 15.c
@@ -19,14 +19,15 @@ int ipower(int n,int k)
   int i,m;
   m=n; 
    
-  for(i=0;i<=k;i++)
-  {
-    if(i==0)
-      printf("%d의 0 제곱 : 1\n",m); 
-    else if(i==1)
-      printf("%d의 1 제곱 : %d\n",m,m);   
-    else 
-      printf("%d의 %d 제곱 : %d\n",m,i,n);
+  for(i=0;i<2;i++)
+  {    
+     if(i==0)
+       printf("%d의 0 제곱 : 1\n",m); 
      
+    else
+      printf("%d의 1 제곱 : %d\n",m,m);   
+    
+
   }
 }
+// ?? 어떠케하지
