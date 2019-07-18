8a4c0ec05aaa44e78fe3c8b8977d847110d122cd
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-18 13:26:35 +0900

    Update 17.c

--- 17.c
+++ 17.c
@@ -4,6 +4,18 @@ double quad_eqn(double a, double b, double c); //근 계산 함수
 
 int main()
 {
+  double a,b,c;
+  printf("이차 방정식 ax²+bx+c=0의 근 구하기\n");
+  printf(" 이차방정식의 계수를 입력해주세요\n");
+  printf(" a : \n");
+  scanf("%lf",&a);
+  printf(" b : \n");
+  scanf("%lf",&b);
+  printf(" c : \n");
+  scanf("%lf",&c);
+  
+  quad_eqn(a,b,c);
+  
 }
 
 double quad_eqn(double a, double b, double c)
