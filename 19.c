1a1674d9706a5758135b169c86df102b0823be2f
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-18 15:43:23 +0900

    Update 19.c

--- 19.c
+++ 19.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+
+double add(double a, double b);
+double sub(double a, double b);
+double mul(double a, double b);
+double div(double a, double b);
+
+
+int main()
+{
+  int a,c;
+  char b;
+  puts("계산기 프로그램");
+  puts("연산을 입력하세요");
+  puts("예: 1+2 ");
+  scanf("%d,%c,%d",&a,&b,&c);
+  
+}
+
 
