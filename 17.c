1693f70a1a9de5744f68188b034392b077ac004d
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-18 13:44:53 +0900

    Update 17.c

--- 17.c
+++ 17.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 double quad_eqn(double a, double b, double c); //근 계산 함수
 
@@ -20,4 +21,19 @@ int main()
 
 double quad_eqn(double a, double b, double c)
 {
+  double root,eqn_p,eqn_m;
+  root=(b*b)-(4*a*c);
+  root=sqrt(root);
   
+  eqn_p=(-b+root)/(2*a);
+  eqn_m=(-b-root)/(2*a);
+  
+  printf("근은 %.2lf,%.2lf 입니다.",eqn_p,eqn_m);
+}
+  
+         
+         
+         
+         
+
+           
