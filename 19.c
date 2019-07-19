c8e04c5b240b9ba68a754c7ca9a7cf1f140f128f
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-19 09:54:09 +0900

    Update 19.c

--- 19.c
+++ 19.c
@@ -22,8 +22,7 @@ int main()
   puts("종료는 0을 눌러주세요.");
   scanf("%lf,%c,%lf",&a,&b,&c);
   
-  while(1)
-  {
+  
     if(b=='+')
   {
     add(a,c);
@@ -36,28 +35,26 @@ int main()
     sub(a,c);
     cnt_s++;
     printf("연산결과: %.2lf\n",sub_1);
-    pritnf("뺄셈은 총 %d번 실행되었습니다.\n",cnt_s);
+    printf("뺄셈은 총 %d번 실행되었습니다.\n",cnt_s);
   }
   else if(b=='*')
   {
     mul(a,c);
     cnt_m++;
     printf("연산결과: %.2lf\n",mul_1);
-    pritnf("곱셈은 총 %d번 실행되었습니다.\n",cnt_m);
+    printf("곱셈은 총 %d번 실행되었습니다.\n",cnt_m);
   }
   else if(b=='/')
   {
     div(a,c);
     cnt_d++;
     printf("연산결과: %.2lf\n",div_1);
-    pritnf("나눗셈은 총 %d번 실행되었습니다.\n",cnt_d);
+    printf("나눗셈은 총 %d번 실행되었습니다.\n",cnt_d);
   }
-    else if(a==0)
-      break;
+     
   else
     printf("정확한 연산자를 입력하세요.");
-}
-  printf("종료합니다.");
+
 }
 
 double add(double a, double b)
@@ -90,3 +87,4 @@ double div(double a, double b)
   return div_1;
 }
 
+// 연산자 
