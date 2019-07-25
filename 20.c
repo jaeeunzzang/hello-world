13d42e8e7c97889e24bd86bf2d236016c22f03a7
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-25 10:06:04 +0900

    Update 20.c

--- 20.c
+++ 20.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+  int i;
+  int a[5];
+  
+  for(i=0;i<5;i++)
+  {
+    a[i]=rand()%10;
+  }
+  
+  for(i=0;i<5;i++)
+  {
+    printf("a[%d]=%d\n",i,a[i]);
+  }
+}
+
+    
