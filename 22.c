8ddec0b80ff34bbae542e493f5f7b121091c2711
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-25 13:54:39 +0900

    Update 22.c

--- 22.c
+++ 22.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#define SIZE 5
 
+int main()
+{
+  int a[SIZE];
+  int i,min;
+  
+  for(i=0;i<SIZE;i++)
+  {
+    printf("성적을 입력하세요:\n");
+    scanf("%d",&a[i]);
+  }
+  
+  min=a[0];
+  
+  for(i=0;i<SIZE;i++)
+  {
+    if(a[i]<min)
+      min=a[i];
+  }
+  
+  printf("최소값은 %d 입니다.",min);
+}
+  
