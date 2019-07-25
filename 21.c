d15fb9b2a968c2ba0bbc3c3163ec7be585597020
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-25 11:21:23 +0900

    Update 21.c

--- 21.c
+++ 21.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  int a[]={10,20,30,40,50,60,70,80,90,100};
+  
+  int i,size;
+  
+  size=sizeof(a)/sizeof(a[0]);
+  
+  for(i=0;i<size;i++)
+  {
+    printf("a[%d]=%d\n",i,a[i]);
+  }
+}
 
