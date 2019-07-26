1346462c45fdb574365a60df6c043049bf623490
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-26 14:51:40 +0900

    Update 23.c

--- 23.c
+++ 23.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
   double x[20];
-  double sum,avg,qns,vus;
+  double pyun[20];
+ 
+  double sum,avg,qns,pyo;
   int i;
   int a=0;
   
@@ -23,15 +26,38 @@ int main()
   a=i; //실수 갯수 a에 저장.
   
   for(i=0;i<a;i++)
-  {
     sum+=x[i];
-  }
-  
+ 
   avg=sum/a;
+ 
   printf("평균은 :%lf\n",avg);
+   //평균구하기
+  
+  puts("편차를 구합니다.");
+  
+  for(i=0;i<a;i++)
+  {
+    pyun[i]=x[i]-avg;
+    printf("%d항의 편차: %lf\n",i+1,pyun[i]);
+    
+    pyun[i]=pow(pyun[i],pyun[i]);
+    printf("%d항의 편차의 제곱: %lf\n",i+1,pyun[i]);
+  }
+  //편차, 편차제곱 구하기
+  
   
   puts("분산을 구합니다");
-  qns=x[i]
+  
+    for(i=0;i<a;i++)
+      qns+=pyun[i];
+    
+    qns=qns/a;
+  printf("분산:%lf\n",qns);
+    //분산구하기
+  
+  pyo=sqrt(qns);
+  printf("표준편차: %lf\n",pyo);
+  
   
   
 }
